85a1668c9f84fa9d7c1a42034c7074a182807258
Stiven Hoxha <stivenhoxha007@gmail.com>
2026-07-20 13:28:07 +0200

    aggiunta layout responsive + immagine sfondo per il mobile

--- src/App.jsx
+++ src/App.jsx
@@ -14,11 +14,13 @@ function App() {
 
         {/* Parte principale */}
         <main>
+
           <div className='hero-content'>
             <span className='text1'>Dal web al tuo player</span>
             <span className='text2'>Converti e Scarica i tuoi brani in pochi secondi</span><br />
             <span className='text3'><em>Ascolta oltre il limite</em></span>
           </div>
+
         </main>
 
 
